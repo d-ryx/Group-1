a4077363ec1f902fd63643da659c4074fe75c761
d-ryx <oderrick186@gmail.com>
2026-04-01 18:30:39 +0300

    Add temperature sensor reading functionality

---
+++ temp.py
@@ -0,0 +1,17 @@
+import machine
+import onewire, ds18x20
+import time
+
+t_pin = machine.Pin(4)
+t_sensor = ds18x20.DS18X20(onewire.OneWire(t_pin))
+scans = t_sensor.scan()
+
+while True:
+    t_sensor.convert_temp()
+    time.sleep_ms(750)
+    
+    for scan in scans:
+        # Read the result from the sensor memory
+        temp = t_sensor.read_temp(scan)
+        print(f"Temperature: {temp:.1f} °C")
+    time.sleep(2)
