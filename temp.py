059a4ff11a4e36ccdca53751b11b32b87bc4d4a0
d-ryx <oderrick186@gmail.com>
2026-04-02 20:51:54 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -19,4 +19,30 @@ while True:
 
 
 # code for distance
+import network
+import socket
+import time
+from machine import Pin, time_pulse_us
 
+# Ultrasonic pins
+trigger = Pin(5, Pin.OUT)
+echo = Pin(18, Pin.IN)
+
+# Tank height (cm)
+TANK_HEIGHT = 100  
+
+def get_distance():
+    trigger.off()
+    time.sleep_ms(2)
+    trigger.on()
+    time.sleep_ms(10)
+    trigger.off()
+    duration = time_pulse_us(echo, 1, 30000)
+    distance = (duration / 2) / 29.1
+    print (f"Distance: {distance:.1f}cm")
+    
+
+while True:
+    dist = get_distance()
+    time.sleep(2)
+    
